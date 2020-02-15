b457560b54d25b1d574544f76c2861535a18f3b6
Altair Wei <altair_wei@outlook.com>
2020-02-15 19:45:05 +0800

    add export option

--- cli.js
+++ cli.js
@@ -1,11 +1,17 @@
-'use strict';
+#!/usr/bin/env node
 
 const dejsc = require('./index.js');
+const path = require('path');
+const fs = require('fs');
+const bytenode = require('bytenode')
 
 let args = process.argv.slice(2);
 
 if (args.includes('-r')) {
   args[args.indexOf('-r')] = '--run';
+}
+if (args.includes('-e')) {
+    args[args.indexOf('-e')] = '--export';
 }
 if (args.includes('-f')) {
   args[args.indexOf('-f')] = '--fix';
@@ -27,6 +33,30 @@ const program = {
     files: args.filter(arg => arg[0] !== '-' && arg[1] !== '-'),
 };
 
+console.log(process.version)
+
 if (program.flags.includes('--run')) {
-    console.log("Hello World");
+    program.files.forEach(function (filename) {
+        filename = path.resolve(filename);
+        if (fs.existsSync(filename) && fs.statSync(filename).isFile()) {
+            let result = dejsc.runBytecodeFile(filename)
+            console.log(result);
+        } else {
+            console.error(`Error: Cannot find file '${filename}'.`);
+        }
+    });
+}
+
+else if (program.flags.includes('--export')) {
+    program.files.forEach(function (filename) {
+        filename = path.resolve(filename);
+        if (fs.existsSync(filename) && fs.statSync(filename).isFile()) {
+            let fakeModule = {};
+            fakeModule.exports = {};
+            (dejsc.runBytecodeFile(filename))(fakeModule.exports, require, fakeModule, __filename, __dirname)
+            console.log(fakeModule.exports);
+        } else {
+            console.error(`Error: Cannot find file '${filename}'.`);
+        }
+    });
 }
